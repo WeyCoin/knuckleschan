5431b3331e81bc84f48bccdbf22a6d5c06f8c239
acidtib <acidtib@protonmail.com>
2018-08-09 22:25:10 -0400

    validating username wip

--- public/js/register.js
+++ public/js/register.js
@@ -2,12 +2,23 @@ $(function() {
   window.Chan.Register = {}
 
   Chan.Register.main = function() {
+    Chan.Register.eyes()
+
     // pre gen a seed for user visiting registration page
     const seed = Chan.createSeed()
     const seedHex = Chan.seedToHex(seed)
     const seedWords = Chan.seedToWords(seed)
     
     $(".seed").text(seedWords)
+  }
+
+  Chan.Register.eyes = function() {
+    $('.register-user').click(function() {
+      console.log($('.username').val())
+      console.log($('.seed').html())
+
+      Chan.Validator.username($('.username').val())
+    })
   }
   
   // get it started
--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,8 +1,18 @@
 $(function() {
   window.Chan = {}
+  window.Chan.Validator = {}
+
+  const Config = {
+    rules: {
+      username: {
+        allow: /^\W+$/, // allows letters, numbers, and underscores
+        length: 6
+      }
+    }
+  }
 
   Chan.main = function() {
-    console.log("hello")
+    
   }
 
   Chan.createSeed = function(bit=96) {
@@ -15,6 +25,17 @@ $(function() {
 
   Chan.seedToWords = function(seed) {
     return seed.toWords()
+  }
+
+  Chan.Validator.username = function(username) {
+    console.log(username)
+    console.log(Config.rules.username.length)
+    
+    if (Config.rules.username.allow.test(username) || username.length < Config.rules.username.length) {
+      console.log('Invalid username');
+    } else {
+      console.log('Valid username');
+    }
   }
 
   // get it started
